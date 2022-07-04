1968d2333ea950868755cc7c3646ca00801caffa
orangejamba <qorwhdqls123@naver.com>
2022-07-04 10:24:38 +0900

    home

--- react-func-class-style-coding/src/App.js
+++ react-func-class-style-coding/src/App.js
@@ -1,8 +1,44 @@
-import logo from "./logo.svg";
 import "./App.css";
+import React, { Component, useState } from "react";
 
 function App() {
-  return <div className="App">Hello React!!</div>;
+  return (
+    <div className="container">
+      <h1>Hello React!!</h1>
+      <FuncComp initNumber={2}></FuncComp>
+      <ClassComp initNumber={2}></ClassComp>
+    </div>
+  );
+}
+
+function FuncComp(props) {
+  return (
+    <div className="container">
+      <h2>function style component</h2>
+      <p>Number:{props.initNumber}</p>
+    </div>
+  );
+}
+
+class ClassComp extends Component {
+  state = {
+    number: this.props.initNumber,
+  };
+  render() {
+    return (
+      <div className="container">
+        <h2>class style component</h2>
+        <p>Number:{this.state.number}</p>
+        <input
+          type="button"
+          value="random"
+          onClick={function () {
+            this.setState({ number: Math.random() });
+          }.bind(this)}
+        ></input>
+      </div>
+    );
+  }
 }
 
 export default App;
