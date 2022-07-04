4461ba8863aaf80dd2ca248c71e9d43c77ddc311
orangejamba <qorwhdqls123@naver.com>
2022-07-04 11:51:09 +0900

    home

--- react-func-class-style-coding/src/App.js
+++ react-func-class-style-coding/src/App.js
@@ -15,28 +15,72 @@ function FuncComp(props) {
   let numberState = useState(props.initNumber);
   let number = numberState[0];
   let setNumber = numberState[1];
+
+  // let dateState = useState(new Date().toString);
+  // let _date = dateState[0];
+  // let setDate = dateState[1];
+
+  let [_date, setDate] = useState(new Date().toString());
+
   return (
     <div className="container">
       <h2>function style component</h2>
-      <p>Number:{number}</p>
+      <p>Number: {number}</p>
+      <p>Date: {_date}</p>
+      <input
+        type="button"
+        value="random"
+        onClick={function () {
+          setNumber(Math.random());
+        }}
+      ></input>
+      <input
+        type="button"
+        value="date"
+        onClick={function () {
+          setDate(new Date().toString());
+        }}
+      ></input>
     </div>
   );
 }
 
+let classStyle = "color:blue";
+
+//constructio -> componentWillMount -> render -> componentDidMount
 class ClassComp extends Component {
   state = {
     number: this.props.initNumber,
+    date: new Date().toString(),
   };
+
+  componentWillMount() {
+    console.log("%cclass => componentWillMount", classStyle);
+  }
+
+  componentDidMount() {
+    console.log("%cclass => componentDidMount", classStyle);
+  }
+
   render() {
+    console.log("%cclass => render", classStyle);
     return (
       <div className="container">
         <h2>class style component</h2>
-        <p>Number:{this.state.number}</p>
+        <p>Number: {this.state.number}</p>
+        <p>Date: {this.state.date}</p>
         <input
           type="button"
           value="random"
           onClick={function () {
             this.setState({ number: Math.random() });
+          }.bind(this)}
+        ></input>
+        <input
+          type="button"
+          value="date"
+          onClick={function () {
+            this.setState({ date: new Date().toString() });
           }.bind(this)}
         ></input>
       </div>
